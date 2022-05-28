b681d8529da6fbe428596157f94831b041441d90
MattyV113 <88385205+MattyV113@users.noreply.github.com>
2022-05-28 16:22:03 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+
+   
+import React from 'react'
+import Courses from '/Users/mattyv113/Desktop/Full-Stack/part1/src/compenents/Courses.js'
+
+const App = ({courses}) =>
+  <div>
+    <Courses courses={courses} />
+  </div>
+
+export default App
