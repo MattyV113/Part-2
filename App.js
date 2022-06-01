1f8c65cc3d89814ace783e136b1b8e11fa1ba240
MattyV113 <88385205+MattyV113@users.noreply.github.com>
2022-06-01 17:44:36 -0400

    Update App.js
    Added axios call with useEffect

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import React,{ useState, useEffect } from 'react'
 import Person from './components_pt2/Person'
 import Filter from './components_pt2/Filter'
 import PersonForm from './components_pt2/PersonForm'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState([]);
   const [filter, setFilter] = useState('')
 
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  },[])
 
   
   const addPerson = (event) => {
@@ -28,13 +31,11 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return;
     }
-
+    
     setPersons(persons.concat(personObject))
     setNewName('')
     setNewNumber('')
     console.log(personObject)
-  
-    
   }
 
   const handleNewPerson = (event) => {
